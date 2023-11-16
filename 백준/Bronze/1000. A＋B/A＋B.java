7c24ca1d26750dd2eaeba86ccd18c2bb13a189c3
seanlancekim0 <122666778+seanlancekim0@users.noreply.github.com>
2023-11-16 17:37:30 +0900

    [Bronze V] Title: A+B, Time: 216 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        int a;
+        int b;
+        
+        Scanner as = new Scanner(System.in);
+        a=as.nextInt();
+        b=as.nextInt();
+        
+        System.out.println(a+b);
+        
+    }
+}
